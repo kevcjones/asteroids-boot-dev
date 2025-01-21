661b5731e91e50fe5229012b9858226721022fa9
Kevin Jones <him@codebykev.dev>
2025-01-21 23:18:36 +0000

    feat: Add player and circle shape classes with basic drawing functionality

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import pygame
-from constants import *
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    startX = SCREEN_WIDTH / 2
+    startY = SCREEN_HEIGHT / 2
+    player = Player(startX, startY)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
